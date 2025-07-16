c16d15c795e0b9de799395676ad88569e23b8243
veer <rupareltech5veer@gmail.com>
2025-07-16 04:23:36 -0700

    fixed module errors and bugs

--- client/app/upload.tsx
+++ client/app/upload.tsx
@@ -35,7 +35,7 @@ export default function UploadScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.8,
